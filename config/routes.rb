e36f3d9e7959192b6f4ee7aa3e00fae32f422aff
eva.laurent.78@gmail.com <eva@MacBook-Pro-de-Eva.local>
2022-06-14 15:37:38 +0200

    controller Breeds created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'breeds/index'
+  get 'breeds/new'
+  get 'breeds/create'
+  get 'breeds/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
